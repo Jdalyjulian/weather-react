150ab2aa8d39736e49829fadfa4f55e492fbd707
Jdalyjulian <jdalyjulian@gmail.com>
2023-06-23 13:11:45 -0500

    added grid loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Grid } from  'react-loader-spinner'
 
 export default function Weather (props){
     function showTemperature(response){
@@ -7,5 +8,14 @@ export default function Weather (props){
     }
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=5f472b7acba333cd8a035ea85a0d4d4c&units=metric`;
     axios.get(apiUrl).then(showTemperature);
-    return <h2>Hello from Weather</h2>;
+    return <Grid
+    height="80"
+    width="80"
+    color="aqua"
+    ariaLabel="grid-loading"
+    radius="12.5"
+    wrapperStyle={{}}
+    wrapperClass=""
+    visible={true}
+  />;
 }
